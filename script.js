d5fd569f11e0ec509377164a9b08c30a0f4c8db9
salahuddin8544 <salahuddinahmed8544@gmail.com>
2022-09-15 06:13:33 +0600

    complete websit

--- script.js
+++ script.js
@@ -115,6 +115,7 @@ const start = () => {
       document.addEventListener("keydown", typeController);
       countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
+      countdownOverlay.innerText ='';
 start
       clearInterval(startCountdown);
       startTime = new Date().getTime();
